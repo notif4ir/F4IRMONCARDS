ca1edff976f5d4c4581feda61e93d494694ca4a8
notif4ir <filiptrz@interia.pl>
2025-06-04 12:33:44 +0200

    Update cardData.js

--- cardData.js
+++ cardData.js
@@ -30,7 +30,7 @@ const CARD_DATA = [
         image: '/c00lkid.png',
         description: 'Popular roblox hacker back in the days, killer from the game "Forsaken" on roblox. He is a very cool kid.',
         rarity: 'legendary',
-        percentage: 2.5
+        percentage: 2
     },
     {
         id: 'johndoe',
@@ -45,8 +45,8 @@ const CARD_DATA = [
         name: 'KJ',
         image: '/KJ.png',
         description: 'Do not ever mess with KJ, this guy can destroy the whole universe in seconds. NAZENARINA! (comes from the KJ Saga on youtube)',
-        rarity: 'rare',
-        percentage: 2.5
+        rarity: 'legendary',
+        percentage: 2
     },
     {
         id: 'jason',
@@ -61,8 +61,8 @@ const CARD_DATA = [
         name: '1x1x1x1',
         image: '/1x1x1x1.png',
         description: 'This is 1eggs from "Forsaken" on roblox, he has an ghost like appearance. He is one of the strongest killers in forsaken, thats why he has so much power in his attacks.',
-        rarity: 'legendary',
-        percentage: 1
+        rarity: 'mythic',
+        percentage: 0.9
     },
     {
         id: 'imnotf4ir',
